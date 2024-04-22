740cfddc3a99ee0cc535e57ae9041936320810cf
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-04-22 10:03:24 +0300

    Refactor CommunityPage component and add styles for header and main section

--- app/community/page.js
+++ app/community/page.js
@@ -1,10 +1,39 @@
 import React from "react";
+import Image from "next/image";
+
+import mealIcon from "@/assets/icons/meal.png";
+import communityIcon from "@/assets/icons/community.png";
+import eventsIcon from "@/assets/icons/events.png";
+import classes from "./page.module.css";
 
 const CommunityPage = () => {
   return (
-    <main>
-      <h1>Community Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion:
+          <span className={classes.hightlight}> accordion Food</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Community Perks</h2>
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt="Meal Icon" />
+            <p>Share & discoer reciper</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="Community Icon" />
+            <p>Find new friends & like-minded people</p>
+          </li>
+          <li>
+            <Image src={eventsIcon} alt="Events Icon" />
+            <p>Participate in exclusive events</p>
+          </li>
+        </ul>
+      </main>
+    </>
   );
 };
 
