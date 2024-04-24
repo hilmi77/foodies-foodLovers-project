a7825b178d65ae6c01dd30ed58e8228f17d91185
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-04-24 13:39:46 +0300

    Refactor loading component and add suspense to MealsPage

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,16 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-const MealsPage = async () => {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +26,9 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<div className={classes.loading}>Loading...</div>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
