751052a2a5f69c7fcdf3b6c99587f7df701da9ae
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-04-24 15:22:33 +0300

    Add error and not-found components for handling errors and page not found

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>Oops! An error occurred.</h1>
+      <p>Fialed to fetch meal data.Please try again later.</p>
+    </main>
+  );
+};
+
+export default Error;
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
