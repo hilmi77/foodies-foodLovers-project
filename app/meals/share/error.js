807c864f0848da0bd8a3872f246d97b3bea4c45d
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-04-29 14:28:11 +0300

    Add Error component for handling errors in sharing meals

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>Oops! An error occurred.</h1>
+      <p>Fialed to create meal...</p>
+    </main>
+  );
+};
+
+export default Error;
